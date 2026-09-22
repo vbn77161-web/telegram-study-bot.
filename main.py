b382e04be6ea531f994d8a3a050485cc28637783
vbn77161-web <vbn77161@gmail.com>
2026-09-22 22:35:49 +0500

    Update main.py

--- main.py
+++ main.py
@@ -49,7 +49,7 @@ def home():
 
 def run_flask():
     port = int(os.environ.get("PORT", 5000))
-    app.run(host="0.0.0.0", port=port)
+    app.run(host="0.0.0.0", port=port, use_reloader=False)
 
 threading.Thread(target=run_flask, daemon=True).start()
 
